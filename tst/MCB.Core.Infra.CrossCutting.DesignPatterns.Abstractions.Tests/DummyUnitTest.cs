e8354f2bfb111e1e815f2c27e0e892e2c3aa287f
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-07 01:10:06 -0300

    feat: add project test with dummy test

---
+++ tst/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Tests/DummyUnitTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Tests
+{
+    public class DummyUnitTest
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
