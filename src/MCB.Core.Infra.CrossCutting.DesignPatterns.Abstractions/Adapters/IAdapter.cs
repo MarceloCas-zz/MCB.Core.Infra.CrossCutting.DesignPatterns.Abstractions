8830dd3e83d40ea76a600064183948bca84c2efd
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-07 00:10:57 -0300

    feat: add adapter pattern abstractions

---
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Adapters/IAdapter.cs
@@ -0,0 +1,7 @@
+﻿namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Adapters
+{
+    public interface IAdapter
+    {
+        TTarget Adapt<TSource, TTarget>(TSource source, TTarget existingTarget);
+    }
+}
