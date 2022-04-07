390ed7d62abf066f7d079e283547abfb1b6c06ee
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-07 01:42:42 -0300

    feat: add covariant support

--- src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Strategy/IStrategy.cs
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Strategy/IStrategy.cs
@@ -1,6 +1,6 @@
 ﻿namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Strategy
 {
-    public interface IStrategy<TInput, TOutput>
+    public interface IStrategy<TInput, out TOutput>
     {
         TOutput Execute(TInput input);
     }
