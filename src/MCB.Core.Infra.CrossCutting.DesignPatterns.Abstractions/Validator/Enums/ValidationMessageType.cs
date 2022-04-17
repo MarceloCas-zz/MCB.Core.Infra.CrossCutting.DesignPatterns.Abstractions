79f48e9b9fca6b326279ac45005ec0dec0b9b035
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-17 02:13:01 -0300

    refact: remove validator because this move to MCB.Core.Infra.CrossCutting.DesignPatterns.Validator.Abstractions package

--- src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Validator/Enums/ValidationMessageType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Validator.Enums
-{
-    public enum ValidationMessageType
-    {
-        Information = 1,
-        Warning = 2,
-        Error = 3
-    }
-}
