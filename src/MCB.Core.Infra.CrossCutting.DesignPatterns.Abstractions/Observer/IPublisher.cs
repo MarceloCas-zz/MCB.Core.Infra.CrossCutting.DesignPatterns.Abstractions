a3ece03c8dcd88afb4c79027a71bbb6232c01d8b
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2022-04-21 02:41:14 -0300

    feat: add Observer abstractions

---
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Observer/IPublisher.cs
@@ -0,0 +1,10 @@
+﻿using MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Observer;
+
+namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Mediator
+{
+    public interface IPublisher
+    {
+        void Subscribe<TSubscriber, TSubject>() where TSubscriber : ISubscriber<TSubject>;
+        void Publish<TSubject>(TSubject subject);
+    }
+}
---
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Observer/ISubscriber.cs
@@ -0,0 +1,11 @@
+﻿namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Observer
+{
+    public interface ISubscriber<in TSubject>
+    {
+        // Properties
+        Type SubjectType { get; }
+
+        // Methods
+        Task HandlerAsync(TSubject subject, CancellationToken cancellationToken);
+    }
+}
