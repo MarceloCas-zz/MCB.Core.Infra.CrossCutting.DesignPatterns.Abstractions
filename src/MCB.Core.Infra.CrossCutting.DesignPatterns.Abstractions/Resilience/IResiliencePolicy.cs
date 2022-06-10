29cc9edfec8fe8cbff58483b55fc23d3925bf33e
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-06-10 03:11:27 -0300

    feat: add input and output parameters

--- src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Resilience/IResiliencePolicy.cs
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Resilience/IResiliencePolicy.cs
@@ -1,24 +1,24 @@
 ﻿using MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Resilience.Enums;
 using MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Resilience.Models;
 
-namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Resilience
+namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Resilience;
+
+public interface IResiliencePolicy
 {
-    public interface IResiliencePolicy
-    {
-        // Properties - Identification
-        string Name { get; }
-        ResilienceConfig ResilienceConfig { get; }
+    // Properties - Identification
+    string Name { get; }
+    ResilienceConfig ResilienceConfig { get; }
 
-        // Properties - Circuit Breaker
-        CircuitState CircuitState { get; }
-        int CurrentRetryCount { get; }
-        int CurrentCircuitBreakerOpenCount { get; }
+    // Properties - Circuit Breaker
+    CircuitState CircuitState { get; }
+    int CurrentRetryCount { get; }
+    int CurrentCircuitBreakerOpenCount { get; }
 
-        // Methods
-        void Configure(Action<ResilienceConfig> configureAction);
-        
-        void CloseCircuitBreakerManually();
-        void OpenCircuitBreakerManually();
-        Task<bool> ExecuteAsync(Func<Task> handler);
-    }
+    // Methods
+    void Configure(Action<ResilienceConfig> configureAction);
+    
+    void CloseCircuitBreakerManually();
+    void OpenCircuitBreakerManually();
+    Task<bool> ExecuteAsync<TInput>(Func<TInput, Task> handler);
+    Task<(bool success, TResult result)> ExecuteAsync<TInput, TResult>(Func<TInput, Task> handler);
 }
