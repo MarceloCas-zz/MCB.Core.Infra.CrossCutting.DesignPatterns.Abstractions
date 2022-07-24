0f1473ed9aba2b26b02dd62306e53e960881cabf
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-07-24 14:09:17 -0300

    refact: remove IFactory inheritante

--- src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Factory/IFactoryWithParameter.cs
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Factory/IFactoryWithParameter.cs
@@ -1,7 +1,6 @@
 ﻿namespace MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Factory;
 
 public interface IFactoryWithParameter<out T, TParameter>
-    : IFactory<T>
 {
     T Create(TParameter parameter);
 }
