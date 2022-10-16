593323c87664b108be9dfa08387f041224932977
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-10-15 23:58:28 -0300

    feat: add adapt overloads

--- src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Adapter/IAdapter.cs
+++ src/MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions/Adapter/IAdapter.cs
@@ -2,8 +2,14 @@
 
 public interface IAdapter
 {
+    object Adapt(Type target, object source);
+    object Adapt(Type target, object source, object existingTarget);
+
     TTarget Adapt<TTarget>(object source);
+    TTarget Adapt<TTarget>(object source, TTarget existingTarget);
+
     object Adapt(object source, object existingTarget);
+
     TTarget Adapt<TSource, TTarget>(TSource source);
     TTarget Adapt<TSource, TTarget>(TSource source, TTarget existingTarget);
 }
